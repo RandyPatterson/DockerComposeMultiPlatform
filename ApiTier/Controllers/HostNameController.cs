565a1520c298f957f3adaca5bf4a6693ea5f34a0
Randy Patterson <you@example.com>
2019-02-13 20:20:35 -0500

    initial checkin

---
+++ ApiTier/Controllers/HostNameController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/hostname")]
+    public class HostNameController : Controller
+    {
+        // GET api/hostname
+        [HttpGet]
+        public string Get()
+        {
+            return Environment.MachineName;
+        }
+
+
+
+
+    }
+}
---
+++ WebTier/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Configuration;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace DockerComposeMulti.Controllers
+{
+    public class HomeController : Controller
+    {
+        [OutputCache(NoStore = true, Location = System.Web.UI.OutputCacheLocation.None)]
+        public async Task<ActionResult> Index()
+        {
+            using (var client = new HttpClient())
+            {
+                //use the environment variable 'apihost' for the base url
+                //if not available, use 'apihost' from web.config
+                var baseUrl = Environment.GetEnvironmentVariable("apihost") ?? ConfigurationManager.AppSettings["apihost"];
+                var url = $"http://{baseUrl}/api/hostname";
+
+                ViewBag.url = url;
+                ViewBag.ApiHostName = await client.GetStringAsync(url);
+            }
+
+            return View();
+        }
+    }
+}
